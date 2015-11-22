a01bf77208acbb9cd6eec0fd57f1eb2bd495ebfa
SeongJae Park <sj38.park@gmail.com>
2015-11-22 23:31:47 +0900

    go: implement basic frame
    ade.go will work as a main program. Android application will be just a
    wrapper of ade.go. With this commit, implement basic frame of ade.go.
    
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- go/ade.go
+++ go/ade.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+const (
+	USAGE = "Usage: ade <command> [argument]"
+	workDir = "ade_work"
+)
+
+func list(args []string) {
+	fmt.Println("list called with... ", args)
+}
+
+func load(args []string) {
+	fmt.Println("load called with... ", args)
+}
+
+func save(args []string) {
+	fmt.Println("save called with...\n", args)
+}
 
 func main() {
 	fmt.Println("ade is a development environment")
+	args := os.Args
+	if len(args) < 2 {
+		fmt.Println("no argument. do nothing")
+		fmt.Println("USAGE: ", USAGE)
+		return
+	}
+	cmd := args[1]
+	args = args[2:]
+	switch cmd {
+	case "list":
+		list(args)
+	case "load":
+		load(args)
+	case "save":
+		save(args)
+	}
 }
